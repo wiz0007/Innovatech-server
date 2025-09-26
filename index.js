367ec764ee750c2c1852bd231181595bf8a27863
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2025-09-26 21:45:44 +0530

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // ✅ Enable CORS
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://innovatech-server.onrender.com"],
+  origin: ["http://localhost:5173", "https://innovatech-puce.vercel.app/"],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
