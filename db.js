117412c02a3addcb8282d86603d32408bc6abe82
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2025-09-26 20:37:56 +0530

    first commit

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
-const dotenv = require("dotenv")
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('MongoDB connected');
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
